4da074a7f1acdb420055250e93f40d9134019270
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-23 13:43:38 +0300

    добавил док с правилами

--- PEP-8_top_30_rules.py
+++ PEP-8_top_30_rules.py
@@ -18,11 +18,10 @@
 # If the implementation is hard to explain, it's a bad idea.
 # If the implementation is easy to explain, it may be a good idea.
 # Namespaces are one honking great idea -- let's do more of those!
-
-
-
+from email import message
 
 # Имена и Переменные
+
 # Имена переменных через snake_case
 # ВЕРНО
 user_name = "Stah"
@@ -32,37 +31,36 @@ total_price = 100
 userName
 TotalPrice
 
+
 # Константы CAPS_LOCK
-
 # CORRECT
-
 MAX_USERS = 100
 PI = 3.1415926
 
 # WRONG
-
 max_users = 100
 pi = 3.1415926
 
-# Классы через CAMEL_CASE
 
+# Классы через CAMEL_CASE
 class UserProfile:
     pass
 
-# Имена должны быть понятнымт
 
+# Имена должны быть понятнымт
 # CORRECT
 user_age = 15
 user_name = "bob"
 
 # WRONG
-
 x = 15
 a = "Bob"
+
 
 # Не используй I O l (и, о, эл) как переменные
 # потому что l похожа на 1
 # буква 0 похожа на цирфру 0
+
 
 # Пробелы spaces
 # Один пробел вокруг операторов
@@ -72,16 +70,44 @@ x = 5 + 3
 # WRONG
 x=5+3
 
-# После запятой ставь пробел
 
+# После запятой ставь пробел
 # CORRECT
 print(a, b, c)
 
-# Не ставь пробелы внутри скобок
 
+# Не ставь пробелы внутри скобок
 # CORRECT
 print(x)
 
 # WRONG
 print( x )
+
+
+# Длина строк
+# в строке не должно быть больше 79-88 символов
+# Если строка огромная то разешен перенос
+message = (
+    "Очень длинный текст"
+    "который лучше перенести"
+)
+
+
+# ФУНКЦИИ
+# между функциями оставляй 2 пустые строки
+def hello():
+    pass
+
+
+def bye():
+    pass
+
+
+# НАЗВАНИЯ ФУНКЦИЙ = ГЛАГОЛЫ
+# CORRECT
+get_user(user_name)
+calculate_total(total_price)
+send_email()
+
+
 
