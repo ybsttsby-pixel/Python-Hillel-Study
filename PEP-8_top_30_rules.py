0429d8e2b3e2fc4e4a895e83769f4d9536f9af17
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-23 13:31:03 +0300

    добавил док с правилами

---
+++ PEP-8_top_30_rules.py
@@ -0,0 +1,63 @@
+# Имена и Переменные
+# Имена переменных через snake_case
+# ВЕРНО
+user_name = "Stah"
+total_price = 100
+
+# НЕ ВЕРНО
+userName
+TotalPrice
+
+# Константы CAPS_LOCK
+
+# CORRECT
+
+MAX_USERS = 100
+PI = 3.1415926
+
+# WRONG
+
+max_users = 100
+pi = 3.1415926
+
+# Классы через CAMEL_CASE
+
+class UserProfile:
+    pass
+
+# Имена должны быть понятнымт
+
+# CORRECT
+user_age = 15
+user_name = "bob"
+
+# WRONG
+
+x = 15
+a = "Bob"
+
+# Не используй I O l (и, о, эл) как переменные
+# потому что l похожа на 1
+# буква 0 похожа на цирфру 0
+
+# Пробелы spaces
+# Один пробел вокруг операторов
+# CORRECT
+x = 5 + 3
+
+# WRONG
+x=5+3
+
+# После запятой ставь пробел
+
+# CORRECT
+print(a, b, c)
+
+# Не ставь пробелы внутри скобок
+
+# CORRECT
+print(x)
+
+# WRONG
+print( x )
+
