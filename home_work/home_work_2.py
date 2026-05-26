460ac463783a129cb59e9dde3433f506dadfa78d
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-27 01:20:05 +0300

    сделал часть дз

--- home_work/home_work_2.py
+++ home_work/home_work_2.py
@@ -13,6 +13,7 @@ print(int(slon) ** 2)
 
 
 # среднее значение трех чисел
+# 1й вариант
 cifra_1 = input("введите первое любое число: ")
 cifra_2 = input("введите второе любое число: ")
 cifra_3 = input("введите третье любое число: ")
@@ -20,18 +21,20 @@ avrg_val = int(cifra_1) + int(cifra_2) + int(cifra_3)
 print(avrg_val / 3)
 
 
+# 2й вариант
 chislo_1 = 6
 chislo_2 = 9
 chislo_3 = 11
 print(float((chislo_1 + chislo_2 + chislo_3) / 3))
 
 
+# 3й вариант
 numero_1 = 40
 numero_2 = 33
 numero_3 = 2
 print(int((numero_1 + numero_2 + numero_3) / 3))
 
-
+# 4й вариант
 import statistics
 avrg_val = [2, 4, 6]
 print(statistics.mean(avrg_val))
