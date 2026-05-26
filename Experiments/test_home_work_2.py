f2e1f6590b3df12ccf370c8ac937b44b98c93280
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-27 00:19:56 +0300

    сделал часть дз

--- Experiments/test_home_work_2.py
+++ Experiments/test_home_work_2.py
@@ -1,20 +1,12 @@
-print("🚨 All Sale- up to 50% 🚨")
-tovari = input("Укажите стомость первого товара: ")
-print("👍")
-tovari_2 = input("Укажите стомость второго товара: ")
-print("👍")
-tovari_3 = input("Укажите стомость третьего товара: ")
-print("👍")
-tovari_total = int(tovari) + int(tovari_2) + int(tovari_3)
-print("Ответьте на доп вопрос и получити доп скидку 40%")
-voprosik = input("Сколько дней в году?: ")
-if voprosik == "365":
-    print("🌟 Win Win Win 🌟",
-          (300 - (tovari_total / 100) * 90), "к оплате")
-else:
-    print("😈 u lose 😈",
-          (300 - (tovari_total / 100) * 50), "к оплате")
-
-
 print("Тест на жадность")
-tovari_sum = input("Введите стомость товара")
+tovari_sum = input("Введите стомость товара: ")
+skidka = input("Какую скидку хотите: от 10 до 100% ?: ")
+tovari_sum = int(tovari_sum)
+skidka = int(skidka)
+if skidka <= 40:
+    print("Good boy!➡️", tovari_sum - skidka, "монеток к оплате")
+if skidka >= 40:
+    print("Zhadina!➡️", tovari_sum - skidka,"монеток к оплате")
+
+
+
--- home_work/home_work_2.py
+++ home_work/home_work_2.py
@@ -67,3 +67,16 @@ else:
     print("😈 u lose 😈",
           (300 - (tovari_total / 100) * 50), "к оплате")
 
+    print("Тест на жадность")
+    tovari_sum = input("Введите стомость товара: ")
+    skidka = input("Какую скидку хотите: от 10 до 100% ?: ")
+    tovari_sum = int(tovari_sum)
+    skidka = int(skidka)
+    if skidka <= 40:
+        print("Good boy!➡️", tovari_sum - skidka, "монеток к оплате")
+    if skidka >= 40:
+        print("Zhadina!➡️", tovari_sum - skidka, "монеток к оплате")
+
+
+
+
