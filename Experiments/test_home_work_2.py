cf9868e27ce73d8baa5cabcf5900b9cb9b737240
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-26 23:02:49 +0300

    сделал часть дз

---
+++ Experiments/test_home_work_2.py
@@ -0,0 +1,7 @@
+vopros = input("Назови число от 100 до 500: ")
+print((int(vopros) / 60))
+
+
+vorpos_2 = input("Сколько в году дней?: ")
+result = (((int(vorpos_2) * 24) * 60))
+print(result, "минут в году")
