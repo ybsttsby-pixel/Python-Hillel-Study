29525905a44e02145711e61bcfbbacacbeb5978d
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-27 01:34:19 +0300

    сделал часть дз

--- Experiments/test_home_work_2.py
+++ Experiments/test_home_work_2.py
@@ -1,5 +1,16 @@
-# превращение минут в часы
-# 1 вариант
-vopros = input("Назови число от 100 до 1000: ")
-znach = ((int(vopros) / 60))
-print((znach), "в минутах")
+print("🚨 All Sale- up to 50% 🚨")
+tovari = input("Укажите стомость первого товара: ")
+print("👍")
+tovari_2 = input("Укажите стомость второго товара: ")
+print("👍")
+tovari_3 = input("Укажите стомость третьего товара: ")
+print("👍")
+tovari_total = int(tovari) + int(tovari_2) + int(tovari_3)
+print("Ответьте на доп вопрос и получити доп скидку 40%")
+voprosik = input("Сколько дней в году?: ")
+if voprosik == "365":
+    print("🌟 Win Win Win 🌟",
+          int((tovari_total / 10) * 0.1), "к оплате")
+else:
+    print("😈 u lose 😈",
+          (300 - (tovari_total / 100) * 50), "к оплате")
