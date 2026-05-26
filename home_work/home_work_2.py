b5562c90e71cb12039d929e664a910fd20edbc5e
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-26 22:39:24 +0300

    старт дз?

--- home_work/home_work_2.py
+++ home_work/home_work_2.py
@@ -1,2 +1,40 @@
+# квадрат числа
 import math
-print(math.sqrt(5))
+slon = input("Введите число: ")
+print(math.sqrt(int(slon)))
+
+
+print(math.sqrt(9))
+
+
+print(3 * 3) # корень с 9
+
+
+# среднее значение трех чисел
+cifra_1 = input("введите первое число: ")
+cifra_2 = input("введите второе число: ")
+cifra_3 = input("введите третье число: ")
+avrg_val = int(cifra_1) + int(cifra_2) + int(cifra_3)
+print(avrg_val / 3)
+
+
+chislo_1 = 6
+chislo_2 = 9
+chislo_3 = 11
+print(float((chislo_1 + chislo_2 + chislo_3) / 3))
+
+
+numero_1 = 40
+numero_2 = 33
+numero_3 = 2
+print(int((numero_1 + numero_2 + numero_3) / 3))
+
+
+import statistics
+avrg_val = [2, 4, 6]
+print(statistics.mean(avrg_val))
+
+
+# превращение минут в часы
+
+
