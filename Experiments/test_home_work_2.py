cd68ce64286f4bfda16fb33b184684b173e60960
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-27 01:00:21 +0300

    сделал часть дз

--- Experiments/test_home_work_2.py
+++ Experiments/test_home_work_2.py
@@ -1,3 +1,11 @@
-age = input("Какой у вас рост?: ")
-puk = int(age) % 10
-print("Длинна члена", puk)
+print("Доставайте ваш прямоугольничек")
+print("Измерили его длину?")
+dlina = input("Введите длину прямоугольника: ")
+print("Измерили его ширину?")
+shirina = input("Введите ширину прямоугольника: ")
+perimetr_1 = 2 * (int(dlina) + int(shirina))
+otvet = input("Хотите узнать результат? Да или Нет?: ")
+if otvet == "Да":
+    print(perimetr_1)
+else:
+    print("(((")
