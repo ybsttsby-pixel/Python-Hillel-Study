6cbf9e9914eddd28aef26e1546289039e262ce9e
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-27 00:34:52 +0300

    сделал часть дз

--- Experiments/test_home_work_2.py
+++ Experiments/test_home_work_2.py
@@ -1,12 +1,3 @@
-print("Тест на жадность")
-tovari_sum = input("Введите стомость товара: ")
-skidka = input("Какую скидку хотите: от 10 до 100% ?: ")
-tovari_sum = int(tovari_sum)
-skidka = int(skidka)
-if skidka <= 40:
-    print("Good boy!➡️", tovari_sum - skidka, "монеток к оплате")
-if skidka >= 40:
-    print("Zhadina!➡️", tovari_sum - skidka,"монеток к оплате")
-
-
-
+age = input("Какой у вас рост?: ")
+puk = int(age) % 10
+print("Длинна члена", puk)
