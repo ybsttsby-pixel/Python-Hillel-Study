a9f7df54d4603627f0dc33cb787a7fa9737b9670
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-26 23:56:05 +0300

    сделал часть дз

--- home_work/home_work_2.py
+++ home_work/home_work_2.py
@@ -49,4 +49,19 @@ otvet = int(vopros_2) * int(vopros_3) * int(vopros_4)
 print((otvet), "минут в году")
 
 
-# 
+# расчет скидки
+print("🚨 All Sale- up to 50% 🚨")
+tovari = input("Укажите стомость первого товара: ")
+tovari_2 = input("Укажите стомость второго товара: ")
+tovari_3 = input("Укажите стомость третьего товара: ")
+tovari_total = int(tovari) + int(tovari_2) + int(tovari_3)
+print("Ответьте на доп вопрос и получити доп скидку 40%")
+voprosik = input("Сколько дней в году?: ")
+if voprosik == "365":
+    print("Win Win Win",
+          (300 - (tovari_total / 100) * 90), "к оплате")
+else:
+    print("u lose",
+          (300 - (tovari_total / 100) * 50), "к оплате")
+
+
