a03dd56e358bf4f1eba27f9fcd18ae862effae87
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-27 01:08:34 +0300

    сделал часть дз

--- Experiments/test_home_work_2.py
+++ Experiments/test_home_work_2.py
@@ -1,11 +1,4 @@
-print("Доставайте ваш прямоугольничек")
-print("Измерили его длину?")
-dlina = input("Введите длину прямоугольника: ")
-print("Измерили его ширину?")
-shirina = input("Введите ширину прямоугольника: ")
-perimetr_1 = 2 * (int(dlina) + int(shirina))
-otvet = input("Хотите узнать результат? Да или Нет?: ")
-if otvet == "Да":
-    print(perimetr_1)
-else:
-    print("(((")
+chi = 6754
+print(chi // 4)
+print(chi % 10)
+
