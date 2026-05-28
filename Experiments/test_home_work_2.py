c198401d0ecd3c3b5ec6d5dbe202e7457b901dda
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-28 09:36:30 +0300

    готовая дз

--- Experiments/test_home_work_2.py
+++ Experiments/test_home_work_2.py
@@ -1,2 +1,11 @@
-num = input('Введите любое число от 1000: ')
-len = (n)
+num = int(input('Введите любое число от 1000: '))
+num_1 = num // 1000
+print(num_1)
+num_2 = num // 100
+num_2_2 = num_2 % 10
+print(num_2_2)
+num_3 = num // 10
+num_3_3 = num_3 % 10
+print(num_3_3)
+num_4 = num % 10
+print(num_4)
