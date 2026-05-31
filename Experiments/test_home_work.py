7d3d16eac5f6f11f3875dcda3bd9852439d15ff1
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-31 20:35:56 +0300

    готовая дз

--- Experiments/test_home_work.py
+++ Experiments/test_home_work.py
@@ -1,13 +1,12 @@
-num_input_1 = int(input('enter first number: '))
-num_input_2 = int(input('enter second number: '))
-operator = input('enter operator: ')
-if num_input_1 == 0 and num_input_2 ==0 and operator == '/':
-    print('ноль на ноль не делится')
-elif operator == '/':
-    print(num_input_1 / num_input_2)
-elif operator == '+':
-    print(num_input_1 + num_input_2)
-elif operator == '-':
-    print(num_input_1 - num_input_2)
-elif operator == ('*'):
-    print(num_input_1 * num_input_2)
+
+lst = [12, 3, 7, 123]
+last = lst.pop()
+lst.insert(0, last)
+print(lst)
+
+
+lst = [12, 3, 7, 123]
+if len(lst) > 1:
+    last = lst.pop()
+    lst.insert(0, last)
+print(lst)
