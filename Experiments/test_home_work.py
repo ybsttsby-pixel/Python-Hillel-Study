1088b0a2941d5402a956821ed1eb534092346647
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-31 19:31:07 +0300

    готовая дз

--- Experiments/test_home_work.py
+++ Experiments/test_home_work.py
@@ -1,16 +1,13 @@
-#Задача 9
 num_input_1 = int(input('enter first number: '))
 num_input_2 = int(input('enter second number: '))
 operator = input('enter operator: ')
-if num_input_1 == 0 and num_input_2 ==0:
+if num_input_1 == 0 and num_input_2 ==0 and operator == '/':
     print('ноль на ноль не делится')
-elif num_input_2 == 0:
-    print('на ноль не делится!')
 elif operator == '/':
     print(num_input_1 / num_input_2)
 elif operator == '+':
     print(num_input_1 + num_input_2)
 elif operator == '-':
     print(num_input_1 - num_input_2)
-elif operator == '*':
+elif operator == ('*'):
     print(num_input_1 * num_input_2)
--- home_work/home_work_3.1.py
+++ home_work/home_work_3.1.py
@@ -29,15 +29,13 @@ else:
 num_input_1 = int(input('enter first number: '))
 num_input_2 = int(input('enter second number: '))
 operator = input('enter operator: ')
-if num_input_1 == 0 and num_input_2 ==0:
+if num_input_1 == 0 and num_input_2 ==0 and operator == '/':
     print('ноль на ноль не делится')
-elif num_input_2 == 0:
-    print('на ноль не делится!')
 elif operator == '/':
     print(num_input_1 / num_input_2)
 elif operator == '+':
     print(num_input_1 + num_input_2)
 elif operator == '-':
     print(num_input_1 - num_input_2)
-elif operator == '*':
+elif operator == ('*'):
     print(num_input_1 * num_input_2)
