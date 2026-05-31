e859cb15587b9a1cbe1906585399806cda8ca53f
stanislavyablonsky <ybsttsby@gmail.com>
2026-05-31 21:01:52 +0300

    готовая дз

--- Experiments/test_home_work.py
+++ Experiments/test_home_work.py
@@ -1,11 +1,12 @@
-
+# 1 вариант
 lst = [12, 3, 7, 123]
 last = lst.pop()
 lst.insert(0, last)
 print(lst)
 
 
-lst = [12, 3, 7, 123]
+# 2 вариант
+lst = []
 if len(lst) > 1:
     last = lst.pop()
     lst.insert(0, last)
